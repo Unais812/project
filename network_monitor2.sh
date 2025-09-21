ed733118d5796c2f36836bb0c8eff91ac262a504
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2025-09-21 16:40:24 +0100

    This is the newest and better version of my network monitor script

--- network_monitor2.sh
+++ network_monitor2.sh
@@ -2,10 +2,24 @@
 
 websites=("google.com" "github.com" "1.1.1.1")
 index=0
+NOW=$(date '+%F_%H:%M:%S')
+
+if [ -d output ];then 
 
 while [ $index -lt ${#websites[@]} ]; do
-echo "checking the latency and reachability of the chosen websites and storing results in YYYY-MM-DD_HHMM.txt.!"
-ping -c 3 ${websites[$index]} >> YYYY-MM-DD_HHMM.txt
-curl -ls ${websites[$index]} >> YYYY-MM-DD_HHMM.txt
+echo "Checking the latency and reachability of for ${websites[index]} and storing results in ${NOW}.txt!"
+ping -c 3 ${websites[$index]} >> output/${NOW}.txt
+curl -ls ${websites[$index]} >> output/${NOW}.txt
 ((index++)) 
-done 
+done
+
+else
+echo "Creating the output directory and storing logs in there"
+mkdir output
+while [ $index -lt ${#websites[@]} ]; do
+echo "Checking the latency and reachability of for ${websites[index]} and storing results in ${NOW}.txt!"
+ping -c 3 ${websites[$index]} >> output/${NOW}.txt
+curl -ls ${websites[$index]} >> output/${NOW}.txt
+((index++)) 
+done
+fi
